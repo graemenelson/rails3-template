0b6be987ded46bf98cdd9dcf292bc06c57b4cb45
Graeme Nelson <graeme@graemenelsonpdx.com>
2010-08-16 16:33:20 -0700

    more updates to the devise routes

--- bootstrap/setup/setup_generator.rb
+++ bootstrap/setup/setup_generator.rb
@@ -18,8 +18,9 @@ module Bootstrap
         # TODO: need to add a root path, for admin it should go to dashboard???
         # see: http://wiki.github.com/plataformatec/devise/howto-redirect-to-a-specific-page-on-successful-sign-in
         route = <<-ROUTE
-          devise_for :#{resource}, :path_names => { :sign_in => 'signin', :sign_out => 'signout' }
+          devise_for :#{resource}, :path_names => { :sign_in => 'signin', :sign_out => 'signout', :sign_up => 'signup' }
           as :#{resource.singularize} do
+            get "/signup" => "devise/registrations#new"
             get "/signin" => "devise/sessions#new"
             get "/signout" => "devise/sessions#destroy"
           end
