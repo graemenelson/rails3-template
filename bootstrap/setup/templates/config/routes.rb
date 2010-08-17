7937c5e62ea5d18645b4424538ce2943b55aecea
Graeme Nelson <graeme@graemenelsonpdx.com>
2010-08-16 17:05:39 -0700

    so cigar on using to_sym in the routes.rb template

--- bootstrap/setup/templates/config/routes.rb
+++ bootstrap/setup/templates/config/routes.rb
@@ -1,7 +1,7 @@
 Rails3Shoulda::Application.routes.draw do
   
-  devise_for <%= @resource.to_sym %>, :path_names => { :sign_in => 'signin', :sign_out => 'signout', :sign_up => 'signup' }
-  as <%= @resource.singularize.to_sym %> do
+  devise_for :<%= @resource %>, :path_names => { :sign_in => 'signin', :sign_out => 'signout', :sign_up => 'signup' }
+  as :<%= @resource.singularize %> do
     get "/signup" => "devise/registrations#new"
     get "/signin" => "devise/sessions#new"
     get "/signout" => "devise/sessions#destroy"
